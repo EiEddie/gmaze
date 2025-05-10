f8f1b345198df874e6d9e90222d1451513ac077a
EICOMPUTER\EiEddie <eieddie4u@outlook.com>
2025-05-10 19:26:58 +0800

    feat: 错误 LED 提示

--- BSP/bspdef.h
+++ BSP/bspdef.h
@@ -5,23 +5,27 @@
 #include <stddef.h>
 
 #include "stm32f1xx_hal.h"
+#include "main.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 
+#define ERROR_Port LED_GPIO_Port
+#define ERROR_Pin  LED_Pin
+
 __attribute__((unused)) static void BSP_Error(const char *file, uint32_t line)
 {
   UNUSED(file);
   UNUSED(line);
-  // TODO: 考虑将某 gpio 置高来说明程序出错
+  HAL_GPIO_TogglePin(ERROR_Port, ERROR_Pin);
   while (1) __NOP();
 }
 
-#define BSP_ASSERT(arg)               \
-  do {                                \
-    if (!(arg))                       \
+#define BSP_ASSERT(arg)              \
+  do {                               \
+    if (!(arg))                      \
       BSP_Error(__FILE__, __LINE__); \
   } while (0)
 
